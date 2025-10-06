7756e74726aa4fe447c634f04ed9da901b7871e3
fabiandiaz416 <diazfabian99@icloud.com>
2025-10-06 16:10:42 -0300

    feat: delete contracts, new naming

---
+++ contracts/programs/private_bridge/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9m6Vt3pBzRCmdAzwxuxkZ3t9dfbFZeTxuTPZegxJcM7s");
+
+#[program]
+pub mod private_bridge {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
