f677b2eab341f24df693d444cd0c2a5abdbc3b53
fabiandiaz416 <diazfabian99@icloud.com>
2025-10-19 11:18:16 -0300

    Update programs/contracts/src/instructions/init.rs

--- contracts/programs/contracts/src/instructions/init.rs
+++ contracts/programs/contracts/src/instructions/init.rs
@@ -1,25 +1,19 @@
+use crate::constants::CIRCUIT_URL_PLAN_PAYOUT;
 use crate::ID;
 use anchor_lang::prelude::*;
 use arcium_anchor::prelude::*;
 use arcium_client::idl::arcium::types::{CircuitSource, OffChainCircuitSource};
 
-
-// This macro implements InitCompDefAccs for the struct
 #[init_computation_definition_accounts("plan_payout", payer)]
 #[derive(Accounts)]
 pub struct InitPlanPayoutCompDef<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
-
-    // PDA del MXE (ya existente, por eso address = derive_mxe_pda!())
     #[account(mut, address = derive_mxe_pda!())]
     pub mxe_account: Box<Account<'info, MXEAccount>>,
-
-    // The comp_def does not exist yet: DO NOT put address constraint here.
     #[account(mut)]
-    /// CHECK: validated by constraint in init_comp_def
+    /// CHECK: validado por init_comp_def
     pub comp_def_account: UncheckedAccount<'info>,
-
     pub arcium_program: Program<'info, Arcium>,
     pub system_program: Program<'info, System>,
 }
@@ -27,10 +21,10 @@ pub struct InitPlanPayoutCompDef<'info> {
 pub fn handler(ctx: Context<InitPlanPayoutCompDef>) -> Result<()> {
     init_comp_def(
         ctx.accounts,
-        true, // allow_untrusted_senders
-        0,    // CU
+        true,
+        0,
         Some(CircuitSource::OffChain(OffChainCircuitSource {
-            source: "https://gateway.pinata.cloud/ipfs/bafybeias7etzkgaaqktjq4tkxb2ptxz4e3qhv5tjzwfk2luufy5pqv5fn4".to_string(),
+            source: CIRCUIT_URL_PLAN_PAYOUT.to_string(),
             hash: [0u8; 32],
         })),
         None,
