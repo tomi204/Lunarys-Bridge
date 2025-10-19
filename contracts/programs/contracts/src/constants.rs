5336087227c1f5632631ec808058b6a8f78f7a42
fabiandiaz416 <diazfabian99@icloud.com>
2025-10-19 11:17:48 -0300

    Update programs/contracts/src/constants.rs

--- contracts/programs/contracts/src/constants.rs
+++ contracts/programs/contracts/src/constants.rs
@@ -1,4 +1,10 @@
 use arcium_anchor::prelude::*;
-
+// Helper to compute comp_def offsets
 pub const COMP_DEF_OFFSET_PLAN_PAYOUT: u32 = comp_def_offset("plan_payout");
-// pub const SIGN_PDA_SEED: &[u8] = b"sign";
+
+pub const COMP_DEF_OFFSET_RESEAL: u32 = comp_def_offset("reseal_destination");
+
+pub const CIRCUIT_URL_PLAN_PAYOUT: &str =
+    "https://gateway.pinata.cloud/ipfs/bafybeibc4lsyclfigtwsdy4julinpfgqynbyfvpkaufjxs3ff5ytcqiswi";
+pub const CIRCUIT_URL_RESEAL: &str =
+    "https://gateway.pinata.cloud/ipfs/bafybeibc4lsyclfigtwsdy4julinpfgqynbyfvpkaufjxs3ff5ytcqiswi";
