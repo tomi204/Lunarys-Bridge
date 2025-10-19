abd81406cf2496aaf93533e2316090e6a60030dc
fabiandiaz416 <diazfabian99@icloud.com>
2025-10-19 11:19:01 -0300

    Add programs/contracts/src/instructions/init_reseal_comp_def.rs

---
+++ contracts/programs/contracts/src/instructions/init_reseal_comp_def.rs
@@ -0,0 +1,33 @@
+use crate::constants::CIRCUIT_URL_RESEAL;
+use crate::ID;
+use anchor_lang::prelude::*;
+use arcium_anchor::prelude::*;
+use arcium_client::idl::arcium::types::{CircuitSource, OffChainCircuitSource};
+
+#[init_computation_definition_accounts("reseal_destination", payer)]
+#[derive(Accounts)]
+pub struct InitResealCompDef<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(mut, address = derive_mxe_pda!())]
+    pub mxe_account: Box<Account<'info, MXEAccount>>,
+    #[account(mut)]
+    /// CHECK: validado por init_comp_def
+    pub comp_def_account: UncheckedAccount<'info>,
+    pub arcium_program: Program<'info, Arcium>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<InitResealCompDef>) -> Result<()> {
+    init_comp_def(
+        ctx.accounts,
+        true,
+        0,
+        Some(CircuitSource::OffChain(OffChainCircuitSource {
+            source: CIRCUIT_URL_RESEAL.to_string(),
+            hash: [0u8; 32],
+        })),
+        None,
+    )?;
+    Ok(())
+}
